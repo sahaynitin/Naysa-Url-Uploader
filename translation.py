908833838f11b22cfe49ec72c685810ab23f0c1b
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2022-01-10 13:24:04 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,4 +1,66 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+class Translation(object):
+
+    START_TEXT = """
+<b>Hey</b><b> {} </b>
+
+<b>I am Telegram Most Powerful Url Uploader Bot</b>
+
+<b>I can Upload Any Link in File or Video</b>
+
+<b>Use Help Command to Know How to Use me</b>
+
+<b>Made With 💕 By</b><b> @Tellybots_4u</b>
+"""
+    HELP_TEXT = """
+<b>Link to Media or File</b>
+➠ <b>Send a link for upload to telegram file or media.</b>
+
+<b>Set Thumbnail</b>
+➠ <b>Send a photo to make it as permanent thumbnail.</b>
+
+<b>Deleting Thumbnail</b>
+➠ Send /delthumb to delete thumbnail.</b>
+
+<b>Show Thumbnail</b>
+➠ Send /showthumb to view custom thumbnail.</b>
+
+<b>Made With 💕 By</b><b> @Tellybots_4u</b>
+"""
+    ABOUT_TEXT = """
+ **🤖 <b>Bot :** URL Uploader</b>\n
+ **👲 <b>Developer :** [Tellybots_4u](https://telegram.me/tellybots_4u)</b>\n
+ **👥 <b>Channel :** [Tellybots_4u](https://telegram.me/tellybots_4u)</b>\n
+ **❄️ <b>Credits :** Everyone in this journey</b>\n
+ **🍴 <b>Source :** [Click here](https://t.me/tellybots_digital)</b>\n
+ **📝 <b>Language :** [Python3](https://python.org)</b>\n
+ **📚 <b>Library :** [Pyrogram v1.2.0](https://pyrogram.org)</b>\n
+ **🌟 <b>Server :** [Heroku](https://heroku.com)</b>\n
+"""
+    START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🤖 Update Channel', url='https://telegram.me/tellybots_4u'),
+        InlineKeyboardButton('💬 Support Group', url='https://telegram.me/tellybots_support')
+        ],[
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('👲 About', callback_data='about'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """𝐈𝐟 𝐲𝐨𝐮 𝐰𝐚𝐧𝐭 𝐭𝐨 𝐝𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐩𝐫𝐞𝐦𝐢𝐮𝐦 𝐯𝐢𝐝𝐞𝐨𝐬, 𝐩𝐫𝐨𝐯𝐢𝐝𝐞 𝐢𝐧 𝐭𝐡𝐞 𝐟𝐨𝐥𝐥𝐨𝐰𝐢𝐧𝐠 𝐟𝐨𝐫𝐦𝐚𝐭:
 URL | filename | username | password"""
