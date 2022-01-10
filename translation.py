d8aa23a31cbcdb9f109b7869d990d95cdd466aea
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2022-01-10 13:23:24 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,48 +1,4 @@
-class Translation(object):
-    START_TEXT = """Hello! 🙋
 
-This is A Powerful Url Uploader Bot that Supports many sites based on YTDLp. 
-
-This Bot can Upload in File and Video Format to Telegram with Permanent Thumbnail Support. 
-
-/help To know how to use me !
-
-Developed with ❤ By @TheTeleRoid 
-"""
-
-    HELP_USER = """𝐈𝐭'𝐬 𝐧𝐨𝐭 𝐭𝐡𝐚𝐭 𝐜𝐨𝐦𝐩𝐥𝐢𝐜𝐚𝐭𝐞𝐝 𝐭𝐨 𝐮𝐬𝐞 𝐦𝐞𝐡! 😅
-    
-✪ 𝐒𝐞𝐧𝐝 𝐌𝐞 𝐀 𝐓𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐢𝐟 𝐫𝐞𝐪𝐮𝐢𝐫𝐞𝐝. 𝐈𝐭'𝐥𝐥 𝐛𝐞 𝐬𝐚𝐯𝐞𝐝 𝐩𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭𝐥𝐲.💯
-✪ 𝐈𝐟 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐖𝐚𝐬𝐧'𝐭 𝐒𝐞𝐭 𝐁𝐲 𝐘𝐨𝐮, 𝐈𝐭'𝐥𝐥 𝐁𝐞 𝐀𝐮𝐭𝐨 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐅𝐫𝐨𝐦 𝐓𝐡𝐞 𝐅𝐢𝐥𝐞.🥳
-✪ 𝐒𝐞𝐧𝐝 𝐌𝐞 𝐀𝐧𝐲 𝐋𝐢𝐧𝐤 𝐓𝐨 𝐁𝐞 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐓𝐨 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦.
-✪ 𝐏𝐫𝐞𝐬𝐬 /𝐝𝐞𝐥𝐞𝐭𝐭𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐓𝐨 𝐃𝐞𝐥𝐞𝐭𝐞 𝐘𝐨𝐮𝐫 𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐂𝐮𝐬𝐭𝐨𝐦 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥.
-✪ 𝐒𝐞𝐥𝐞𝐜𝐭 𝐭𝐡𝐞 𝐛𝐮𝐭𝐭𝐨𝐧.
-   𝐒𝐕𝐢𝐝𝐞𝐨 - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐚𝐬 𝐯𝐢𝐝𝐞𝐨 𝐰𝐢𝐭𝐡 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
-   𝐃𝐅𝐢𝐥𝐞  - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐰𝐢𝐭𝐡 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
-   𝐕𝐢𝐝𝐞𝐨  - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐚𝐬 𝐯𝐢𝐝𝐞𝐨 𝐰𝐢𝐭𝐡𝐨𝐮𝐭 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
-   𝐃𝐅𝐢𝐥𝐞  - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐰𝐢𝐭𝐡𝐨𝐮𝐭 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
-𝐍𝐁 : 𝐈𝐭 𝐢𝐬 𝐑𝐞𝐜𝐨𝐦𝐦𝐞𝐧𝐝𝐞𝐝 𝐓𝐨 𝐔𝐬𝐞 𝐀 𝐂𝐮𝐬𝐭𝐨𝐦 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐁𝐞𝐜𝐚𝐮𝐬𝐞, 𝐒𝐨𝐦𝐞 𝐓𝐢𝐦𝐞 𝐖𝐨𝐧'𝐭 𝐔𝐩𝐥𝐨𝐚𝐝 𝐓𝐡𝐞 𝐅𝐢𝐥𝐞 𝐖𝐢𝐭𝐡𝐨𝐮𝐭 𝐚 𝐂𝐮𝐬𝐭𝐨𝐦 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥.
-𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐆𝐫𝐨𝐮𝐩 : @TeleRoid14
-"""
-    ABOUT_TEXT = """<b>🤖 My Name: <a href='https://t.me/URLUploaderV3Bot'> @URLUploaderV3Bot </a></b>
-
-<b>👨‍💻 Developer :<a href='https://t.me/PredatorHackerzZ'>@TheTeleRoid</a></b>
-
-<b>📝 Language: 𝐏𝐲𝐭𝐡𝐨𝐧𝟑</b>
-
-<b>📡 Server: <a href='https://www.heroku.com'> Heroku </a></b>
-
-<b>📕 Library: Pyrogram 1.0.7</b>
-
-<b>💢 Source Code:<a href='https://github.com/PredatorHackerzZ/UPLOADER-BOT'> Click Here </a></b>
-
-<b>📢 Bot Support:<a href='https://t.me/TeleRoid14'> @TeleRoid14</a></b>
-
-<b>🔔 Bot Updates: <a href='https://t.me/TeleRoidGroup'> @TeleRoidGroup</a></b>
-
-<b>🌀 Telegram BotList: <a href='https://t.me/TGRobot_List'> @TGRobot_List</a></b>
-
-<b>🚸 Powered By: <a href='https://t.me/MoviesFlixers_DL'> @HindiWebNetwok</a></b>"""
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """𝐈𝐟 𝐲𝐨𝐮 𝐰𝐚𝐧𝐭 𝐭𝐨 𝐝𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐩𝐫𝐞𝐦𝐢𝐮𝐦 𝐯𝐢𝐝𝐞𝐨𝐬, 𝐩𝐫𝐨𝐯𝐢𝐝𝐞 𝐢𝐧 𝐭𝐡𝐞 𝐟𝐨𝐥𝐥𝐨𝐰𝐢𝐧𝐠 𝐟𝐨𝐫𝐦𝐚𝐭:
 URL | filename | username | password"""
