94ee59389995cd6245e98fb726677e50e82197a9
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2022-01-11 16:38:15 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -37,13 +37,13 @@ async def echo(bot, update):
         try:
             user = await bot.get_chat_member(Config.UPDATE_CHANNEL, update.chat.id)
             if user.status == "kicked":
-              await bot.edit_message_text(text=Translation.BANNED_USER_TEXT, message_id=fmsg.message_id)
+              await bot.edit_message_text(text=Translation.BANNED_USER_TEXT, message_id=chk.message_id)
               return
         except UserNotParticipant:
-            await bot.edit_message_text(chat_id=update.chat.id, text=Translation.FORCE_SUBSCRIBE_TEXT, message_id=fmsg.message_id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="😎 Join Channel 😎", url=f"https://telegram.me/{Config.UPDATE_CHANNEL}")]]))
+            await bot.edit_message_text(chat_id=update.chat.id, text=Translation.FORCE_SUBSCRIBE_TEXT, message_id=chk.message_id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="😎 Join Channel 😎", url=f"https://telegram.me/{Config.UPDATE_CHANNEL}")]]))
             return
         except Exception:
-            await bot.edit_message_text(chat_id=update.chat.id, text=Translation.SOMETHING_WRONG, message_id=fmsg.message_id)
+            await bot.edit_message_text(chat_id=update.chat.id, text=Translation.SOMETHING_WRONG, message_id=chk.message_id)
             return
         if str(update.from_user.id) in Config.ADL_BOT_RQ:
             current_time = time.time()
@@ -52,7 +52,7 @@ async def echo(bot, update):
             present_time = round(Config.PROCESS_MAX_TIMEOUT-(current_time - previous_time))
             Config.ADL_BOT_RQ[str(update.from_user.id)] = time.time()
             if round(current_time - previous_time) < Config.PROCESS_MAX_TIMEOUT:
-                await bot.edit_message_text(chat_id=update.chat.id, text=Translation.FREE_USER_LIMIT_Q_SZE.format(process_max_timeout, present_time), disable_web_page_preview=True, parse_mode="html", message_id=fmsg.message_id)
+                await bot.edit_message_text(chat_id=update.chat.id, text=Translation.FREE_USER_LIMIT_Q_SZE.format(process_max_timeout, present_time), disable_web_page_preview=True, parse_mode="html", message_id=chk.message_id)
                 return
         else:
             Config.ADL_BOT_RQ[str(update.from_user.id)] = time.time()
