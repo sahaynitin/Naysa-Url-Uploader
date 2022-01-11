b93b4040a77cbc2bb084d3d7a56f15fc4e7e5d4d
Tellybots <93035329+Tellybots@users.noreply.github.com>
2022-01-11 12:37:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -7,8 +7,12 @@ from translation import Translation
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
+    if not update.from_user:
+        return await update.reply_text("I don't know about you sar :(")
+    await add_user_to_database(bot, update)
     await update.reply_text(
         text=Translation.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
