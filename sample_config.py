1427a7d1b78b47c581b83a3140e2b19a7a944146
Tellybots <93035329+Tellybots@users.noreply.github.com>
2022-01-11 12:44:28 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -36,4 +36,8 @@ class Config(object):
     SESSION_NAME = os.environ.get("SESSION_NAME", "")
     # database uri (mongodb)
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    
+    PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
+    PRO_USERS.append(OWNER_ID)
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
