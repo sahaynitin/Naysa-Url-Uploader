98f803c187a8bc9c2ad23f15c4bbb74063cb29d7
Tellybots <93035329+Tellybots@users.noreply.github.com>
2022-01-11 11:52:35 +0530

    Update help_Nemko_ffmpeg.py

--- helper_funcs/help_Nemko_ffmpeg.py
+++ helper_funcs/help_Nemko_ffmpeg.py
@@ -66,35 +66,7 @@ async def place_water_mark(input_file, output_file, water_mark_file):
     return output_file
 
 
-async def take_screen_shot(video_file, output_directory, ttl):
-    # https://stackoverflow.com/a/13891070/4723940
-    out_put_file_name = output_directory + \
-        "/" + str(time.time()) + ".jpg"
-    file_genertor_command = [
-        "ffmpeg",
-        "-ss",
-        str(ttl),
-        "-i",
-        video_file,
-        "-vframes",
-        "1",
-        out_put_file_name
-    ]
-    # width = "90"
-    process = await asyncio.create_subprocess_exec(
-        *file_genertor_command,
-        # stdout must a pipe to be accessible as process.stdout
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    t_response = stdout.decode().strip()
-    if os.path.lexists(out_put_file_name):
-        return out_put_file_name
-    else:
-        return None
+a
 
 # https://github.com/Nekmo/telegram-upload/blob/master/telegram_upload/video.py#L26
 
