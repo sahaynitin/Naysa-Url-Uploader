9a708e407572dbbd38a5ba4cfca036b6ea071d25
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2022-01-11 16:41:33 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -124,3 +124,4 @@ Free users only 1 request per 30 minutes.
 /upgrade or Try 1800 seconds later."""
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
     FORCE_SUBSCRIBE_TEXT = "<code>Sorry Dear You Must Join My Updates Channel for using me 😌😉....</code>"
+    BANNED_USER_TEXT = "<code>You are Banned!</code>"
