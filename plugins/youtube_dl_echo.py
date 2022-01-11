f2a524d293a98a991879afe63d2730d5f226e57b
Tellybots <93035329+Tellybots@users.noreply.github.com>
2022-01-11 14:55:51 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -189,11 +189,7 @@ async def echo(bot, update):
                 cb_string_file = "{}|{}|{}|{}".format(
                     "file", format_id, format_ext, randem)
                 if format_string is not None and not "audio only" in format_string:
-                    ikeyboard = [
-                        InlineKeyboardButton(
-                "🎥 Video", callback_data=f"(cb_string_video).encode("UTF-8")||{format_ext}||{format_string}||{approx_file_size}"), InlineKeyboardButton("Document", callback_data=f"(cb_string_file).encode("UTF-8")||{format_ext}||{format_string}||{approx_file_size}")
-                                                                                                    
-                    ]
+                    ikeyboard = [InlineKeyboardButton("🎥 Video", callback_data="(cb_string_video).encode("UTF-8")||{format_ext}||{format_string}||{approx_file_size}"), InlineKeyboardButton("Document", callback_data="(cb_string_file).encode("UTF-8")||{format_ext}||{format_string}||{approx_file_size}")]
                     """if duration is not None:
                         cb_string_video_message = "{}|{}|{}|{}|{}".format(
                             "vm", format_id, format_ext, ran, randem)
